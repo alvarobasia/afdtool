a7fd10996777bd887815a15a506fcdd958064249
Helena Brito Dias <helenabdias95@gmail.com>
2021-12-23 20:24:09 -0300

    add afd.c

---
+++ src/domain/features/afd/afd.c
@@ -0,0 +1,96 @@
+#include <stdlib.h>
+#include <string.h>
+#include "./afd.h"
+
+Transition *getTransition(AFD afd, char *from, char *to, char *read)
+{
+    int fromPosition = getStatePosition(afd, from);
+    int readPosition = getSymbolPosition(afd, read);
+    int toPosition = getStatePosition(afd, to);
+
+    Transition *transition = malloc(sizeof(Transition));
+    transition->from = malloc(sizeof(int));
+    transition->read = malloc(sizeof(int));
+    transition->to = malloc(sizeof(int));
+
+    *transition->from = fromPosition;
+    *transition->read = readPosition;
+    *transition->to = toPosition;
+
+    return transition;
+}
+
+Transition *getEmptyTransition()
+{
+    Transition *transition = malloc(sizeof(Transition));
+    transition->from = malloc(sizeof(int));
+    transition->read = malloc(sizeof(int));
+    transition->to = malloc(sizeof(int));
+}
+
+int getStatePosition(AFD afd, char *state)
+{
+    for (int i = 0; i < *afd.qtd_states; i++)
+    {
+        if (!strcmp(afd.states[i], state))
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int getSymbolPosition(AFD afd, char *symbol)
+{
+    for (int i = 0; i < *afd.qtd_symbols; i++)
+    {
+        if (!strcmp(afd.alphabet[i], symbol))
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+AFD *getEmptyAFD()
+{
+    AFD *afd = malloc(sizeof(AFD));
+    afd->qtd_states = malloc(sizeof(int));
+    afd->qtd_symbols = malloc(sizeof(int));
+    afd->qtd_transitions = malloc(sizeof(int));
+    afd->qtd_final_states = malloc(sizeof(int));
+    afd->initial_state = malloc(sizeof(int));
+}
+
+void freeTransition(Transition *transition)
+{
+    free(transition->from);
+    free(transition->to);
+    free(transition->read);
+}
+
+void freeAFD(AFD *afd)
+{
+    for (int i = 0; i < *afd->qtd_states; i++)
+    {
+        free(afd->states[i]);
+    }
+    for (int i = 0; i < *afd->qtd_symbols; i++)
+    {
+        free(afd->alphabet[i]);
+    }
+    for (int i = 0; i < *afd->qtd_transitions; i++)
+    {
+        freeTransition(afd->transitions[i]);
+    }
+
+    free(afd->transitions);
+    free(afd->alphabet);
+    free(afd->states);
+    free(afd->final_states);
+    free(afd->qtd_states);
+    free(afd->qtd_symbols);
+    free(afd->qtd_final_states);
+    free(afd->qtd_transitions);
+    free(afd->initial_state);
+}
