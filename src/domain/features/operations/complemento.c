1a63faa64898be6224d286fa942640d0ebc85088
naalmeida98 <natii_98_@hotmail.com>
2021-12-27 15:35:23 -0300

    função complemento

--- src/domain/features/operations/complemento.c
+++ src/domain/features/operations/complemento.c
@@ -1,12 +1,31 @@
 #include <string.h>
-#include "../AFD/afd.h"
+#include "../afd/afd.c"
 
 AFD *complementacao(AFD *entrada){
 
     AFD *ent = entrada;
     AFD *comp = *ent;
 
-    for(int i; i<ent->qtdEstFinal; i++){
-        if(ent->estados[])
+    //limpando os estados finais
+    comp->final_states = NULL;
+    int cont = 0, position = 0;
+
+    // verificando quais estados não são finais no AFD de entrada e adicionando-os como
+    // estados finais no AFD de complemento
+
+    for(int i; i<ent->qtd_states; i++){
+        for(int j; j<ent->qtd_final_states; j++){
+            if(ent->states[i] != ent->final_states[j]){
+                cont++;
+            }
+        }
+        if(cont == ent->qtd_final_states){
+            comp->final_states[position] = ent->states[i];
+            position++;
+        }
+        cont=0;
     }
+
+    free(ent);
+    return comp;
 }
