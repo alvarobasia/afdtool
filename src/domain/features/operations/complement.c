c3c9fdbf62526cd0b0282624d90cb672e4eaf35b
naalmeida98 <natii_98_@hotmail.com>
2021-12-28 20:46:27 -0300

    Update complement.c

--- src/domain/features/operations/complement.c
+++ src/domain/features/operations/complement.c
@@ -7,27 +7,33 @@ void *complementacao(AFD *entrada, AFD *complemento)
 {
     AFD *ent = getEmptyAFD();
     ent = entrada;
-    AFD *comp = getEmptyAFD();
-    comp = ent;
+    
+
+    // ent->final_states = malloc(sizeof(char) * entrada->qtd_final_states);
+    // ent->final_states = entrada->final_states;
 
     printf("\n--------estados de entrada:\n%s\n%s", ent->states[0], ent->states[1]);
     printf("\n--------estado final entrada:\n%s", ent->final_states[0]);
-    printf("\n--------estados de complemento\n%s\n%s", comp->states[0], comp->states[1]);
 
     //limpando os estados finais
-    comp->final_states = NULL;
-    comp->qtd_final_states = ent->qtd_states-ent->qtd_final_states;
-    comp->final_states = malloc(sizeof(char) * comp->qtd_final_states);
-    int cont = 0, position = 0;
+    // comp->final_states = NULL;
+    int comp_qtd_final_states = ent->qtd_states-ent->qtd_final_states;
+    char **comp_final_states = malloc(sizeof(char) * comp_qtd_final_states);
+    int cont = 0, position = 0, size;
+    char *state, *newState;
+             
+    
 
     // verificando quais estados não são finais no AFD de entrada e adicionando-os como
     // estados finais no AFD de complemento
 
     for (int i=0; i < ent->qtd_states; i++)
     {
+        printf("\n1");
         for (int j=0; j < ent->qtd_final_states; j++)
         {
-            if (strcmp(ent->states[i],ent->final_states[j]) != 0)
+            printf("\n2");
+            if (!(strcmp(ent->states[i],ent->final_states[j]) == 0))
             {   
                 printf("\nentrou 1");
                 cont++;
@@ -36,14 +42,25 @@ void *complementacao(AFD *entrada, AFD *complemento)
         if (cont == ent->qtd_final_states)
         {   
             printf("\nentrou 2");
-            comp->final_states[position] = ent->states[i];
+            printf("\nestado final para entrar: %s",ent->states[i]);
+            state = ent->states[i];
+            
+            size = strlen(state);
+            newState = malloc((size + 1) * sizeof(char));
+            strcpy(newState, state);
+            printf("\nnewState: %s", newState);
+            strcpy(comp_final_states[position],newState);
+            //comp_final_states[position] = newState;
+            printf("\nestado final dentro: %s", comp_final_states[position]);
             position++;
-            printf("\nestado final dentro: %s", comp->final_states[position]);
+            
         }
         cont = 0;
     }
 
-    complemento = comp;
+    complemento = ent;
+    complemento->final_states = comp_final_states;
+
     freeAFD(ent);
-    freeAFD(comp);
+    free(comp_final_states);
 }
